d6afa5994b845290b5e9a7a60835133849ce312d
ichdream <chensheng0802@outlook.com>
2021-08-24 16:01:25 +0800

    add data generate description

--- Topics/Sort/codes/sortMain.cpp
+++ Topics/Sort/codes/sortMain.cpp
@@ -12,6 +12,7 @@ int main()
     int n = 1000000;
     vector<int> arr1;
     // 生成含 1000000 个数据在 [0, 1000000] 的数组
+    printf("随机数据测试，测试数据量100w.\n");
     sort_helper::generate_array(arr1, n, 0, n);
     sort_helper::testsort<int>("mergesort", arr1, merge_sort<int>);
     sort_helper::generate_array(arr1, n, 0, n);
@@ -25,6 +26,7 @@ int main()
 
     // 生成含 1000000 个数据在 [0, 1000000] 近乎有序的数组
     // vector<int> arr3;
+    printf("近乎有序数据测试，测试数据量100w.\n");
     sort_helper::generate_nearly_sorted_array(arr1, n, 10);
     sort_helper::testsort<int>("mergesort", arr1, merge_sort<int>);
     sort_helper::generate_nearly_sorted_array(arr1, n, 10);
@@ -37,6 +39,7 @@ int main()
     sort_helper::testsort<int>("stl sort", arr1, stlSort<int>);
 
     // 生成含 1000000 个数据在 [0, 10] 的数组
+    printf("大量重复数据测试，测试数据量100w.\n");
     sort_helper::generate_array(arr1, n, 0, 10);
     sort_helper::testsort<int>("mergesort", arr1, merge_sort<int>);
     sort_helper::generate_array(arr1, n, 0, 10);
